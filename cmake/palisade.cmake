5b37c9a909bd0e172d2b82ba27ec452460e009cf
Jack Crawford <jack.crawford@intel.com>
2021-05-25 14:25:44 +0100

    Update palisade cmake

--- cmake/palisade.cmake
+++ cmake/palisade.cmake
@@ -42,7 +42,7 @@ else()
   set(PALISADE_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext_palisade)
   set(PALISADE_SRC_DIR ${PALISADE_PREFIX}/src/ext_palisade/)
   set(PALISADE_REPO_URL https://gitlab.com/palisade/palisade-development.git)
-  set(PALISADE_GIT_TAG 457640e7)
+  set(PALISADE_GIT_TAG 28f933e9)
 
   if (ENABLE_INTEL_HEXL)
     ExternalProject_Add(
