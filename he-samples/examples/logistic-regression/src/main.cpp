52dc10bc805348e132671fdd875612082f8ad11e
Sejun Kim <sejun.kim@intel.com>
2021-07-19 10:58:10 -0700

    - Simplified docompare - Changed to counts mismatches if initial comparison fails

--- he-samples/examples/logistic-regression/src/main.cpp
+++ he-samples/examples/logistic-regression/src/main.cpp
@@ -76,18 +76,23 @@ int main(int argc, char** argv) {
   if (FLAGS_docompare) {  // If docompare==true, compare with non-HE inference
                           // result
     // Get cleartext inference results
-    std::vector<double> lr_weights = lrhe.get_weights();
-    double lr_bias = lrhe.get_bias();
-    auto lrcleartext_evalout = lrhelper::test(evalData, lr_weights, lr_bias);
+    auto lrcleartext_evalout =
+        lrhelper::test(evalData, pretrained_weights, pretrained_bias);
 
-    // Count mismatch
-    int match_ct = n_inputs;
-    for (size_t j = 0; j < n_inputs; ++j) {
-      if ((lrhe_evalout[j] - lrcleartext_evalout[j]) != 0.0) {
-        match_ct--;
+    // Check for match
+    if (lrhe_evalout == lrcleartext_evalout) {
+      LOG<Info>("All match!");
+    } else {
+      // Count mismatch
+      int mismatch_ct = 0;
+      for (size_t j = 0; j < n_inputs; ++j) {
+        if (lrhe_evalout[j] != lrcleartext_evalout[j]) {
+          mismatch_ct++;
+        }
       }
+      LOG<Info>("Mismatch count with cleartext LR:", mismatch_ct, "/",
+                n_inputs);
     }
-    LOG<Info>("Match count with cleartext LR:", match_ct, "out of", n_inputs);
   }
   return 0;
 }
