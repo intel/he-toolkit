3a0dd911a594c5d655539a32243ee4a8c6238258
Jack Crawford <jack.crawford@intel.com>
2021-08-26 09:24:41 -0700

    Removed micro-kernels runners

--- docker/runners/run_micro_kernels_seal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copyright (C) 2020-2021 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-set -e
-
-"$HOME"/he-samples/build/micro-kernels/micro-kernels-seal --benchmark_min_time=2
