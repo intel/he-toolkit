4ee4b560c0d7c1422c4f66aa804d779761d9e6ac
Fabian Boemer <fabian.boemer@intel.com>
2021-10-19 16:16:39 -0700

    Cleaner SEAL dependency

--- he-samples/cmake/seal.cmake
+++ he-samples/cmake/seal.cmake
@@ -1,77 +1,41 @@
 # Copyright (C) 2020-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-include(ExternalProject)
+include(FetchContent)
 
 option(SEAL_PREBUILT OFF) # Set to ON/OFF to use prebuilt installation
 message(STATUS "SEAL_PREBUILT: ${SEAL_PREBUILT}")
 
-if (SEAL_PREBUILT) # Skip download from gitlab
-  if (ENABLE_INTEL_HEXL)
-    find_package(HEXL 1.2.1 HINTS ${INTEL_HEXL_HINT_DIR} REQUIRED)
-  endif()
-  find_package(SEAL 3.7
-    HINTS ${SEAL_HINT_DIR}
-    REQUIRED)
-  add_library(libseal ALIAS SEAL::seal)
-  # TODO(fboemer): Support pre-built shared seal
+if (SEAL_PREBUILT) # Skip download from github
+  find_package(SEAL 3.7 HINTS ${SEAL_HINT_DIR} REQUIRED)
 else()
-  set(SEAL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext_seal)
-  set(SEAL_SRC_DIR ${SEAL_PREFIX}/src/ext_seal/)
-  set(SEAL_REPO_URL https://github.com/microsoft/SEAL.git)
-  set(SEAL_GIT_TAG v3.7.0)
+  FetchContent_Declare(
+    seal
+    PREFIX seal
+    GIT_REPOSITORY https://github.com/microsoft/SEAL.git
+    GIT_TAG v3.7.1
+  )
+  FetchContent_GetProperties(seal)
 
-  set(SEAL_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+  if(NOT seal_POPULATED)
+    FetchContent_Populate(seal)
+    set(CMAKE_C_COMPILER ${CMAKE_C_COMPILER} CACHE STRING "" FORCE)
+    set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER} CACHE STRING "" FORCE)
+    set(CMAKE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX} CACHE STRING "" FORCE)
+    set(SEAL_SHARED_LIB OFF CACHE BOOL "" FORCE)
+    set(SEAL_USE_CXX17 ON CACHE BOOL "" FORCE)
+    set(EXCLUDE_FROM_ALL TRUE)
 
-  set(SEAL_SHARED_LIB OFF) # Set to ON/OFF to toggle shared build
+    set(SEAL_USE_INTEL_HEXL ${ENABLE_INTEL_HEXL})
 
-  if (ENABLE_INTEL_HEXL)
-    ExternalProject_Add(
-      ext_seal
-      GIT_REPOSITORY ${SEAL_REPO_URL}
-      GIT_TAG ${SEAL_GIT_TAG}
-      PREFIX ${SEAL_PREFIX}
-      INSTALL_DIR ${SEAL_PREFIX}
-      CMAKE_ARGS ${BENCHMARK_FORWARD_CMAKE_ARGS}
-        -DCMAKE_CXX_FLAGS=${SEAL_CXX_FLAGS}
-        -DCMAKE_INSTALL_PREFIX=${SEAL_PREFIX}
-        -DSEAL_USE_CXX17=ON
-        -DCMAKE_INSTALL_LIBDIR=${SEAL_PREFIX}/lib
-        -DCMAKE_INSTALL_INCLUDEDIR=${SEAL_PREFIX}/include
-        -DSEAL_USE_INTEL_HEXL=${ENABLE_INTEL_HEXL}
-        -DBUILD_SHARED_LIBS=${SEAL_SHARED_LIB}
-      # Skip updates
-      UPDATE_COMMAND ""
-      )
-  else()
-    ExternalProject_Add(
-      ext_seal
-      GIT_REPOSITORY ${SEAL_REPO_URL}
-      GIT_TAG ${SEAL_GIT_TAG}
-      PREFIX ${SEAL_PREFIX}
-      INSTALL_DIR ${SEAL_PREFIX}
-      CMAKE_ARGS ${BENCHMARK_FORWARD_CMAKE_ARGS}
-        -DCMAKE_INSTALL_PREFIX=${SEAL_PREFIX}
-        -DSEAL_USE_CXX17=ON
-        -DCMAKE_INSTALL_LIBDIR=${SEAL_PREFIX}/lib
-        -DCMAKE_INSTALL_INCLUDEDIR=${SEAL_PREFIX}/include
-        -DSEAL_USE_INTEL_HEXL=${ENABLE_INTEL_HEXL}
-        -DBUILD_SHARED_LIBS=${SEAL_SHARED_LIB}
-      # Skip updates
-      UPDATE_COMMAND ""
-      )
+    mark_as_advanced(BUILD_SEAL)
+    mark_as_advanced(INSTALL_SEAL)
+    mark_as_advanced(FETCHCONTENT_SOURCE_DIR_SEAL)
+    mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_SEAL)
+
+    add_subdirectory(
+        ${seal_SOURCE_DIR}
+        EXCLUDE_FROM_ALL
+    )
   endif()
-
-  ExternalProject_Get_Property(ext_seal SOURCE_DIR BINARY_DIR)
-
-  add_library(libseal INTERFACE)
-  add_dependencies(libseal ext_seal)
-  target_include_directories(libseal INTERFACE ${SEAL_PREFIX}/include/SEAL-3.7)
-
-  if (SEAL_SHARED_LIB)
-      target_link_libraries(libseal INTERFACE ${SEAL_PREFIX}/lib/libseal.so.3.7)
-  else()
-      target_link_libraries(libseal INTERFACE ${SEAL_PREFIX}/lib/libseal-3.7.a)
-  endif()
-
 endif()
