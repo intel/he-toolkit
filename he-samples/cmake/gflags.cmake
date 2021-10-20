a187edcb344a29c11c2a96d576156759cfc27970
Fabian Boemer <fabian.boemer@intel.com>
2021-10-19 18:07:39 -0700

    FetchContent for gflags

--- he-samples/cmake/gflags.cmake
+++ he-samples/cmake/gflags.cmake
@@ -1,30 +1,29 @@
 # Copyright (C) 2020-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-include(ExternalProject)
+include(FetchContent)
 
-set(GFLAGS_GIT_REPO_URL https://github.com/gflags/gflags.git)
-set(GFLAGS_GIT_LABEL v2.2.2)
+FetchContent_Declare(
+  gflags
+  PREFIX gflags
+  GIT_REPOSITORY https://github.com/gflags/gflags.git
+  GIT_TAG v2.2.2
+)
+FetchContent_GetProperties(gflags)
 
-ExternalProject_Add(
-  ext_gflags
-  PREFIX ext_gflags
-  GIT_REPOSITORY ${GFLAGS_GIT_REPO_URL}
-  GIT_TAG ${GFLAGS_GIT_LABEL}
-  INSTALL_COMMAND ""
-  UPDATE_COMMAND ""
-  EXCLUDE_FROM_ALL TRUE)
+if(NOT gflags_POPULATED)
+  FetchContent_Populate(gflags)
+  set(CMAKE_C_COMPILER ${CMAKE_C_COMPILER} CACHE STRING "" FORCE)
+  set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER} CACHE STRING "" FORCE)
+  set(CMAKE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX} CACHE STRING "" FORCE)
 
-# ------------------------------------------------------------------------------
+  mark_as_advanced(BUILD_gflags)
+  mark_as_advanced(INSTALL_gflags)
+  mark_as_advanced(FETCHCONTENT_SOURCE_DIR_gflags)
+  mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_gflags)
 
-ExternalProject_Get_Property(ext_gflags SOURCE_DIR BINARY_DIR)
-
-add_library(libgflags INTERFACE)
-add_dependencies(libgflags ext_gflags)
-message(STATUS "libgflags include: ${BINARY_DIR}/include/")
-message(STATUS "libgflags library: ${BINARY_DIR}/lib/")
-
-target_include_directories(libgflags SYSTEM
-                           INTERFACE ${BINARY_DIR}/include)
-target_link_libraries(libgflags
-                      INTERFACE ${BINARY_DIR}/lib/libgflags.a)
+  add_subdirectory(
+      ${gflags_SOURCE_DIR}
+      EXCLUDE_FROM_ALL
+  )
+endif()
