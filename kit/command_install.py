556d37ca9f4ecde79e40097905aeeaf2b6477d8a
Hamish Hunt <hamish.hunt@intel.com>
2022-01-20 08:05:25 -0800

    fix some notices

--- kit/command_install.py
+++ kit/command_install.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2021 Intel Corporation
+# Copyright (C) 2020-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 from component_builder import components_to_build_from, chain_run
