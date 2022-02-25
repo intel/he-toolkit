c1d3d6d32efc5103f0a76e881319f140fc86d13e
Jack Crawford <jack.crawford@intel.com>
2022-02-25 10:47:05 -0800

    Initial cleanup of CMake

--- he-samples/cmake/helib.cmake
+++ he-samples/cmake/helib.cmake
@@ -1,92 +1,13 @@
 # Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-include(ExternalProject)
+#FIXME: Need logic for setting HINT DIR
+set(HELIB_HINT_DIR ${HEKIT_DIR}/components/helib/v2.2.1/install
+  CACHE PATH "Location of HElib install")
+message(STATUS "HELIB_HINT_DIR: ${HELIB_HINT_DIR}")
 
-option(HELIB_PREBUILT OFF) # Set to ON/OFF to use prebuilt installation
-message(STATUS "HELIB_PREBUILT: ${HELIB_PREBUILT}")
+find_package(helib
+  HINTS ${HELIB_HINT_DIR}
+  REQUIRED)
 
-if (HELIB_PREBUILT) # Skip download from GitHub
-
-  message(STATUS "HELIB_HINT_DIR: ${HELIB_HINT_DIR}")
-  find_package(helib
-    HINTS ${HELIB_HINT_DIR}
-    REQUIRED)
-
-  add_library(libhelib ALIAS helib)
-
-else()
-
-  set(HELIB_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext_helib)
-  set(HELIB_SRC_DIR ${HELIB_PREFIX}/src/ext_helib/)
-  set(HELIB_REPO_URL https://github.com/homenc/HElib.git)
-  set(HELIB_GIT_TAG v2.2.1)
-
-  set(HELIB_CXX_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
-
-  set(HELIB_SHARED_LIB OFF)
-
-  if(ENABLE_INTEL_HEXL)
-    ExternalProject_Add(
-      ext_helib
-      GIT_REPOSITORY ${HELIB_REPO_URL}
-      GIT_TAG ${HELIB_GIT_TAG}
-      PREFIX ${HELIB_PREFIX}
-      INSTALL_DIR ${HELIB_PREFIX}
-      CMAKE_ARGS ${BENCHMARK_FORWARD_CMAKE_ARGS}
-      -DCMAKE_CXX_FLAGS=${HELIB_CXX_FLAGS}
-      -DCMAKE_INSTALL_PREFIX=${HELIB_PREFIX}
-      -DCMAKE_INSTALL_LIBDIR=${HELIB_PREFIX}/lib
-      -DCMAKE_INSTALL_INCLUDEDIR=${HELIB_PREFIX}/include
-      -DBUILD_SHARED=${HELIB_SHARED_LIB}
-      -DPACKAGE_BUILD=OFF
-      -DENABLE_THREADS=ON
-      -DHELIB_DEBUG=OFF
-      -DENABLE_TEST=OFF
-      -DNTL_DIR=${NTL_DIR}
-      -DGMP_DIR=${GMP_DIR}
-      -DUSE_INTEL_HEXL=${ENABLE_INTEL_HEXL}
-      -DHEXL_DIR=${INTEL_HEXL_HINT_DIR}
-      -DPEDANTIC_BUILD=ON
-      # Skip updates
-      UPDATE_COMMAND ""
-      )
-  else(ENABLE_INTEL_HEXL)
-    ExternalProject_Add(
-      ext_helib
-      GIT_REPOSITORY ${HELIB_REPO_URL}
-      GIT_TAG ${HELIB_GIT_TAG}
-      PREFIX ${HELIB_PREFIX}
-      INSTALL_DIR ${HELIB_PREFIX}
-      CMAKE_ARGS ${BENCHMARK_FORWARD_CMAKE_ARGS}
-      -DCMAKE_CXX_FLAGS=${HELIB_CXX_FLAGS}
-      -DCMAKE_INSTALL_PREFIX=${HELIB_PREFIX}
-      -DCMAKE_INSTALL_LIBDIR=${HELIB_PREFIX}/lib
-      -DCMAKE_INSTALL_INCLUDEDIR=${HELIB_PREFIX}/include
-      -DBUILD_SHARED=${HELIB_SHARED_LIB}
-      -DPACKAGE_BUILD=OFF
-      -DENABLE_THREADS=ON
-      -DHELIB_DEBUG=OFF
-      -DENABLE_TEST=OFF
-      -DNTL_DIR=${NTL_DIR}
-      -DGMP_DIR=${GMP_DIR}
-      -DUSE_INTEL_HEXL=OFF
-      -DPEDANTIC_BUILD=ON
-      # Skip updates
-      UPDATE_COMMAND ""
-      )
-  endif()
-
-  ExternalProject_Get_Property(ext_helib SOURCE_DIR BINARY_DIR)
-
-  add_library(libhelib INTERFACE)
-  add_dependencies(libhelib ext_helib)
-  target_include_directories(libhelib INTERFACE ${HELIB_PREFIX}/include/)
-
-  if (HELIB_SHARED_LIB)
-    target_link_libraries(libhelib INTERFACE ${HELIB_PREFIX}/lib/libhelib.so ${NTL_DIR}libntl.so ${GMP_DIR}libgmp.so)
-  else()
-    target_link_libraries(libhelib INTERFACE ${HELIB_PREFIX}/lib/libhelib.a ${NTL_DIR}libntl.a ${GMP_DIR}libgmp.so)
-  endif()
-
-endif()
+add_library(libhelib ALIAS helib)
--- he-samples/cmake/seal.cmake
+++ he-samples/cmake/seal.cmake
@@ -1,77 +1,17 @@
 # Copyright (C) 2020 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-include(ExternalProject)
+#FIXME: Need logic for setting HINT DIR
+set(SEAL_HINT_DIR ${HEKIT_DIR}/components/seal/v3.7.2/install
+  CACHE PATH "Location of SEAL install")
+message(STATUS "SEAL_HINT_DIR: ${SEAL_HINT_DIR}")
 
-option(SEAL_PREBUILT OFF) # Set to ON/OFF to use prebuilt installation
-message(STATUS "SEAL_PREBUILT: ${SEAL_PREBUILT}")
+# Set path to dependencies
+set(Microsoft.GSL_DIR ${HEKIT_DIR}/components/gsl/v3.1.0/install/share/cmake/Microsoft.GSL)
+set(zstd_DIR ${HEKIT_DIR}/components/zstd/v1.4.5/install/lib/cmake/zstd)
 
-if (SEAL_PREBUILT) # Skip download from gitlab
-  if (ENABLE_INTEL_HEXL)
-    find_package(HEXL 1.2.3 HINTS ${INTEL_HEXL_HINT_DIR} REQUIRED)
-  endif()
-  find_package(SEAL 3.7
-    HINTS ${SEAL_HINT_DIR}
-    REQUIRED)
-  add_library(libseal ALIAS SEAL::seal)
-  # TODO(fboemer): Support pre-built shared seal
-else()
-  set(SEAL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext_seal)
-  set(SEAL_SRC_DIR ${SEAL_PREFIX}/src/ext_seal/)
-  set(SEAL_REPO_URL https://github.com/microsoft/SEAL.git)
-  set(SEAL_GIT_TAG v3.7.2)
+find_package(SEAL
+  HINTS ${SEAL_HINT_DIR}
+  REQUIRED)
 
-  set(SEAL_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
-
-  set(SEAL_SHARED_LIB OFF) # Set to ON/OFF to toggle shared build
-
-  if (ENABLE_INTEL_HEXL)
-    ExternalProject_Add(
-      ext_seal
-      GIT_REPOSITORY ${SEAL_REPO_URL}
-      GIT_TAG ${SEAL_GIT_TAG}
-      PREFIX ${SEAL_PREFIX}
-      INSTALL_DIR ${SEAL_PREFIX}
-      CMAKE_ARGS ${BENCHMARK_FORWARD_CMAKE_ARGS}
-        -DCMAKE_CXX_FLAGS=${SEAL_CXX_FLAGS}
-        -DCMAKE_INSTALL_PREFIX=${SEAL_PREFIX}
-        -DSEAL_USE_CXX17=ON
-        -DCMAKE_INSTALL_LIBDIR=${SEAL_PREFIX}/lib
-        -DCMAKE_INSTALL_INCLUDEDIR=${SEAL_PREFIX}/include
-        -DSEAL_USE_INTEL_HEXL=${ENABLE_INTEL_HEXL}
-        -DBUILD_SHARED_LIBS=${SEAL_SHARED_LIB}
-      # Skip updates
-      UPDATE_COMMAND ""
-      )
-  else()
-    ExternalProject_Add(
-      ext_seal
-      GIT_REPOSITORY ${SEAL_REPO_URL}
-      GIT_TAG ${SEAL_GIT_TAG}
-      PREFIX ${SEAL_PREFIX}
-      INSTALL_DIR ${SEAL_PREFIX}
-      CMAKE_ARGS ${BENCHMARK_FORWARD_CMAKE_ARGS}
-        -DCMAKE_INSTALL_PREFIX=${SEAL_PREFIX}
-        -DSEAL_USE_CXX17=ON
-        -DCMAKE_INSTALL_LIBDIR=${SEAL_PREFIX}/lib
-        -DCMAKE_INSTALL_INCLUDEDIR=${SEAL_PREFIX}/include
-        -DSEAL_USE_INTEL_HEXL=${ENABLE_INTEL_HEXL}
-        -DBUILD_SHARED_LIBS=${SEAL_SHARED_LIB}
-      # Skip updates
-      UPDATE_COMMAND ""
-      )
-  endif()
-
-  ExternalProject_Get_Property(ext_seal SOURCE_DIR BINARY_DIR)
-
-  add_library(libseal INTERFACE)
-  add_dependencies(libseal ext_seal)
-  target_include_directories(libseal INTERFACE ${SEAL_PREFIX}/include/SEAL-3.7)
-
-  if (SEAL_SHARED_LIB)
-      target_link_libraries(libseal INTERFACE ${SEAL_PREFIX}/lib/libseal.so.3.7)
-  else()
-      target_link_libraries(libseal INTERFACE ${SEAL_PREFIX}/lib/libseal-3.7.a)
-  endif()
-
-endif()
+add_library(libseal ALIAS SEAL::seal)
