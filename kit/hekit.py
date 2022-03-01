d35323ed235f98df36a0238e45e7608b9cb151a0
Dennis Calderon <dennis.calderon.vega@intel.com>
2022-03-01 12:05:36 -0800

    Fixes per code review

--- kit/hekit.py
+++ kit/hekit.py
@@ -9,29 +9,13 @@ from argparse import ArgumentParser
 from config import load_config
 from command_init import init_hekit
 from command_remove import remove_components
-from command_list import list_components, get_components, get_instances
+from command_list import list_components
 from command_install import install_components
-
-
-def autocomplete_cmdline(parser):
-    try:
-        from argcomplete import autocomplete
-
-        autocomplete(parser)
-    except ImportError:
-        # ignore error if not installed
-        pass
-
-
-def parse_components(prefix, parsed_args, **kwargs):
-    parsed_args.config = load_config(parsed_args.config)
-    return get_components(parsed_args.config.repo_location)
-
-
-def parse_instances(prefix, parsed_args, **kwargs):
-    parsed_args.config = load_config(parsed_args.config)
-    comp_name_path = f"{parsed_args.config.repo_location}/{parsed_args.component}"
-    return get_instances(comp_name_path)
+from tab_completion import (
+    enable_tab_completion,
+    components_completer,
+    instances_completer,
+)
 
 
 def parse_cmdline():
@@ -87,13 +71,13 @@ def parse_cmdline():
     )
     parser_remove.add_argument(
         "component", type=str, help="component to be removed"
-    ).completer = parse_components
+    ).completer = components_completer
     parser_remove.add_argument(
         "instance", type=str, help="instance to be removed"
-    ).completer = parse_instances
+    ).completer = instances_completer
     parser_remove.set_defaults(fn=remove_components)
 
-    autocomplete_cmdline(parser)
+    enable_tab_completion(parser)
 
     return parser.parse_args(), parser.print_help
 
---
+++ kit/tab_completion.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from config import load_config
+from command_list import get_components, get_instances
+
+
+def enable_tab_completion(parser):
+    try:
+        from argcomplete import autocomplete
+
+        autocomplete(parser)
+    except ImportError:
+        pass
+
+
+def components_completer(prefix, parsed_args, **kwargs):
+    parsed_args.config = load_config(parsed_args.config)
+    return get_components(parsed_args.config.repo_location)
+
+
+def instances_completer(prefix, parsed_args, **kwargs):
+    parsed_args.config = load_config(parsed_args.config)
+    comp_name_path = f"{parsed_args.config.repo_location}/{parsed_args.component}"
+    return get_instances(comp_name_path)
