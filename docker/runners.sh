81b9393c269d098ccc9e7be4bf805a16e7e0cc04
Hamish Hunt <hamish.hunt@intel.com>
2022-04-01 06:02:05 -0700

    reword welcome msg

--- docker/runners.sh
+++ docker/runners.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
@@ -60,28 +58,31 @@ welcome_message() {
   cat << EOF
 
 There are several bash functions sourced that you can run,
-  1. run_sample_kernels_[palisade|seal]: This will run several HE sample kernels including
-     Matrix Multiplication and Logistic Regression. The script will display Wall time,
-     CPU time, and the number of iterations that were run.
-  2. run_tests: This will run several unit tests to confirm the validity of the above
-     sample kernels by comparing against the same operation in the non-HE space.
+  1. run_sample_kernels_[palisade|seal]: This will run several HE sample
+     kernels including Matrix Multiplication and Logistic Regression. The
+     script will display Wall time, CPU time, and the number of iterations that
+     were run.
+  2. run_tests: This will run several unit tests to confirm the validity of the
+     above sample kernels by comparing against the same operation in the non-HE
+     space.
   3. run_query_example: This will run a "Secure Query" example allowing users
-     to query on a database of the 50 U.S. States while controlling (optionally) the
-     crypto-parameters used. When prompted, enter a State and, if present, the
-     corresponding City will be decoded and printed.
-  4. run_lr_example: This will run a "Logistic Regression" (LR) example allowing users
-     to see a faster and more scalable method for LR in HE. Unlike the LR code available
-     before in the sample-kernels, this version takes extra steps to utilize as many slots
-     as possible in the ciphertexts.
-  5. run_psi_example: this will execute an example that uses the HElib library and the
-     BGV scheme to compute the intersection of two given sets. The program requires as input,
-     a file with the client set, this is mandatory parameter, so the user must create this
-     file before executing the example. Use the flag "-h" for usage information.
+     to query on a database of the 50 U.S. States while controlling
+     (optionally) the crypto-parameters used. When prompted, enter a State and,
+     if present, the corresponding City will be decoded and printed.
+  4. run_lr_example: This will run a "Logistic Regression" (LR) example
+     allowing users to see a faster and more scalable method for LR in HE.
+     Unlike the LR code available before in the sample-kernels, this version
+     takes extra steps to utilize as many slots as possible in the ciphertexts.
+  5. run_psi_example: this will execute an example that uses the HElib library
+     and the BGV scheme to compute the intersection of two given sets. The
+     program requires as input, a file with the client set, this is mandatory
+     parameter, so the user must create this file before executing the example.
+     Use the flag "-h" for usage information.
 
-The bash functions are run via command line by name, e.g. run_tests. When
-done testing, to terminate the docker with the "exit" command or temporarily leave
-the docker Ctrl+p -> Ctrl+q, then re-enter using docker exec (see "Docker Controls" in
-the User Guide for more information).
+The bash functions are run via command line by name, e.g. run_tests. When done,
+you can terminate the docker container with the "exit" command or temporarily
+leave the docker container typing Ctrl+p then Ctrl+q. To re-enter a detached
+docker container you can reattach using \`docker attach <container-id|container-name>\`.
 
 
 EOF
