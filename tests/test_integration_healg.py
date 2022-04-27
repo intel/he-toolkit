e8b82669b01a63ee68efd92f74ecdc4497afef8f
Dennis Calderon <dennis.calderon.vega@intel.com>
2022-04-27 12:18:11 -0700

    Fixing integraion tests

--- tests/test_integration_healg.py
+++ tests/test_integration_healg.py
@@ -3,7 +3,7 @@
 
 import pytest
 from .context import healg
-from healg import parse_cmdline, main 
+from healg import parse_cmdline, main
 
 
 def test_main_arg_header(mocker):
@@ -17,11 +17,12 @@ def test_main_arg_header(mocker):
 
     """Act"""
     args = parse_cmdline(cmdline_args)
+    main(args)
 
     """Assert"""
-    assert main(args) is None
     assert 2 == mock_print.call_count
     mock_print.assert_called_with(exp_output)
+
 
 def test_main_arg_no_header(mocker):
     """Arrange"""
@@ -32,9 +33,9 @@ def test_main_arg_no_header(mocker):
 
     """Act"""
     args = parse_cmdline(cmdline_args)
+    main(args)
 
     """Assert"""
-    assert main(args) is None
     mock_print.assert_not_called()
 
 
@@ -49,26 +50,27 @@ def test_main_arg_p(mocker):
 
     """Act"""
     args = parse_cmdline(cmdline_args)
+    main(args)
 
     """Assert"""
-    assert main(args) is None
     assert 3 == mock_print.call_count
     mock_print.assert_called_with(exp_output)
+
 
 def test_main_arg_d(mocker):
     """Arrange"""
     cmdline_args = "-p 2 -d 4-5 --no-header".split()
     width = 20
-    exp_output = f"{'2' :^{width}} {'5' :^{width}} {'31' :^{width}} {'30' :^{width}} {'6' :^{width}}"   
+    exp_output = f"{'2' :^{width}} {'5' :^{width}} {'31' :^{width}} {'30' :^{width}} {'6' :^{width}}"
     mock_PrimesFromFile = mocker.patch("healg.PrimesFromFile")
     mock_PrimesFromFile.is_prime.return_value = True
     mock_print = mocker.patch("healg.print")
 
     """Act"""
     args = parse_cmdline(cmdline_args)
+    main(args)
 
     """Assert"""
-    assert main(args) is None
     assert 4 == mock_print.call_count
     mock_print.assert_called_with(exp_output)
 
@@ -77,16 +79,15 @@ def test_main_arg_no_corrected(mocker):
     """Arrange"""
     cmdline_args = "-p 11-25 -d 2,4-5 --no-corrected --no-header".split()
     width = 20
-    exp_output = f"{'25' :^{width}} {'5' :^{width}} {'9765624' :^{width}} {'2912000' :^{width}} {'582400' :^{width}}"   
+    exp_output = f"{'25' :^{width}} {'5' :^{width}} {'9765624' :^{width}} {'2912000' :^{width}} {'582400' :^{width}}"
     mock_PrimesFromFile = mocker.patch("healg.PrimesFromFile")
     mock_PrimesFromFile.is_prime.return_value = True
     mock_print = mocker.patch("healg.print")
 
     """Act"""
     args = parse_cmdline(cmdline_args)
+    main(args)
 
     """Assert"""
-    assert main(args) is None
     assert 786 == mock_print.call_count
     mock_print.assert_called_with(exp_output)
-
