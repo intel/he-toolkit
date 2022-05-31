415d3adcd93da672174f6094a11c247de6da0b28
Hamish Hunt <hamish.hunt@intel.com>
2022-05-31 14:03:50 -0700

    move util functions out

--- kit/hekit.py
+++ kit/hekit.py
@@ -11,49 +11,19 @@ import sys
 
 assert sys.version_info >= (3, 8)
 
-from os import geteuid, walk
+from os import geteuid
 from sys import stderr, exit as sys_exit
-from importlib import import_module
 from argparse import ArgumentParser
 from pathlib import Path
-from typing import List
 
 from commands.init import init_hekit
 from tools.healg import healg
+from utils.subparsers import discover_subparsers_from
 from utils.constants import Constants  # pylint: disable=no-name-in-module
 from utils.config import load_config  # pylint: disable=no-name-in-module
 from utils.tab_completion import (  # pylint: disable=no-name-in-module
     enable_tab_completion,
 )
-
-
-def files_in_dir(path: str, cond) -> List[str]:
-    """Returns a list of filenames in the directory given by path. Can be filtered by cond"""
-    try:
-        filenames = next(walk(path))[2]
-        if cond is None:
-            return filenames
-        return list(filter(cond, filenames))
-    except StopIteration:
-        return []
-
-
-def discover_subparsers_from(module_dirs: List[str], kit_root: str):
-    """Import modules in module_dirs, and discover and return a generator of set_.*_subparser functions"""
-    for module_dir in module_dirs:
-        filenames = files_in_dir(
-            f"{kit_root}/{module_dir}", lambda f: f[0] != "_" and f.endswith(".py")
-        )
-        imported_modules = (
-            import_module(f"{module_dir}.{fname[:-3]}") for fname in filenames
-        )
-        funcs = (
-            getattr(imported_module, funcname)
-            for imported_module in imported_modules
-            for funcname in dir(imported_module)
-            if funcname.startswith("set_") and funcname.endswith("_subparser")
-        )
-        yield from funcs
 
 
 def parse_cmdline():
