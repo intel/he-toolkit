3d1c8ba99196663dc53c7d8c585949419e3187ab
Dennis Calderon <dennis.calderon.vega@intel.com>
2022-06-07 09:39:54 -0700

    Fixing subparsers test

--- tests/test_subparsers.py
+++ tests/test_subparsers.py
@@ -10,39 +10,49 @@ from subparsers import files_in_dir, discover_subparsers_from
 
 def test_discover_subparsers_from_commands_all():
     # Arrange
+    exp_func = {
+        "set_docker_subparser",
+        "set_check_dep_subparser",
+        "set_install_subparser",
+        "set_init_subparser",
+        "set_remove_subparser",
+        "set_new_subparser",
+        "set_list_subparser",
+    }
     hekit_root_dir = Path(__file__).resolve().parent.parent
 
     # Act
-    func = discover_subparsers_from(["commands"], hekit_root_dir / "kit")
+    act_funcs = discover_subparsers_from(["commands"], hekit_root_dir / "kit")
 
     # Assert
-    assert next(func).__name__ == "set_docker_subparser"
-    assert next(func).__name__ == "set_check_dep_subparser"
-    assert next(func).__name__ == "set_install_subparser"
-    assert next(func).__name__ == "set_init_subparser"
-    assert next(func).__name__ == "set_remove_subparser"
-    assert next(func).__name__ == "set_new_subparser"
-    assert next(func).__name__ == "set_list_subparser"
+    for func in act_funcs:
+        func_name = func.__name__
+        assert func_name in exp_func
+        exp_func.remove(func_name)
+    assert 0 == len(exp_func)
 
 
 def test_discover_subparsers_from_commands_some(mocker):
     # Arrange
+    exp_func = {"set_check_dep_subparser", "set_remove_subparser", "set_list_subparser"}
     hekit_root_dir = Path(__file__).resolve().parent.parent
     mocker_files_in_dir = mocker.patch("subparsers.files_in_dir")
     mocker_files_in_dir.return_value = ["check_deps.py", "remove.py", "list_cmd.py"]
 
     # Act
-    func = discover_subparsers_from(["commands"], hekit_root_dir / "kit")
+    act_funcs = discover_subparsers_from(["commands"], hekit_root_dir / "kit")
 
     # Assert
-    assert next(func).__name__ == "set_check_dep_subparser"
-    assert next(func).__name__ == "set_remove_subparser"
-    assert next(func).__name__ == "set_list_subparser"
+    for func in act_funcs:
+        func_name = func.__name__
+        assert func_name in exp_func
+        exp_func.remove(func_name)
+    assert 0 == len(exp_func)
 
 
 def test_files_in_dir_commands_filter_py(mocker):
     # Arrange
-    exp_files = [
+    exp_files = {
         "docker_build.py",
         "check_deps.py",
         "install.py",
@@ -50,7 +60,7 @@ def test_files_in_dir_commands_filter_py(mocker):
         "remove.py",
         "new.py",
         "list_cmd.py",
-    ]
+    }
     hekit_root_dir = Path(__file__).resolve().parent.parent
     module = hekit_root_dir / "kit" / "commands"
     filter = lambda f: f[0] != "_" and f.endswith(".py")
@@ -59,12 +69,12 @@ def test_files_in_dir_commands_filter_py(mocker):
     filenames = files_in_dir(module, filter)
 
     # Assert
-    assert filenames == exp_files
+    assert exp_files == set(filenames)
 
 
 def test_files_in_dir_commands_filter_None(mocker):
     # Arrange
-    exp_files = [
+    exp_files = {
         "docker_build.py",
         "__init__.py",
         "check_deps.py",
@@ -73,7 +83,7 @@ def test_files_in_dir_commands_filter_None(mocker):
         "remove.py",
         "new.py",
         "list_cmd.py",
-    ]
+    }
     hekit_root_dir = Path(__file__).resolve().parent.parent
     module = hekit_root_dir / "kit" / "commands"
     filter = None
@@ -82,26 +92,30 @@ def test_files_in_dir_commands_filter_None(mocker):
     filenames = files_in_dir(module, filter)
 
     # Assert
-    assert filenames == exp_files
+    assert exp_files == set(filenames)
 
 
 def test_discover_subparsers_from_tools_some(mocker):
     # Arrange
+    exp_func = {"set_gen_algebras_subparser", "set_gen_primes_subparser"}
     hekit_root_dir = Path(__file__).resolve().parent.parent
     mocker_files_in_dir = mocker.patch("subparsers.files_in_dir")
     mocker_files_in_dir.return_value = ["healg.py"]
 
     # Act
-    func = discover_subparsers_from(["tools"], hekit_root_dir / "kit")
+    act_funcs = discover_subparsers_from(["tools"], hekit_root_dir / "kit")
 
     # Assert
-    assert next(func).__name__ == "set_gen_algebras_subparser"
-    assert next(func).__name__ == "set_gen_primes_subparser"
+    for func in act_funcs:
+        func_name = func.__name__
+        assert func_name in exp_func
+        exp_func.remove(func_name)
+    assert 0 == len(exp_func)
 
 
 def test_files_in_dir_tools_filter_py(mocker):
     # Arrange
-    exp_files = ["healg.py"]
+    exp_files = {"healg.py"}
     hekit_root_dir = Path(__file__).resolve().parent.parent
     module = hekit_root_dir / "kit" / "tools"
     filter = lambda f: f[0] != "_" and f.endswith(".py")
@@ -110,4 +124,4 @@ def test_files_in_dir_tools_filter_py(mocker):
     filenames = files_in_dir(module, filter)
 
     # Assert
-    assert filenames == exp_files
+    assert exp_files == set(filenames)
