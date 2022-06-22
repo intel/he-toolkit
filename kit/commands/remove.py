447505e8531d535f698b019fa86e45dd1571289a
Hamish Hunt <hamish.hunt@intel.com>
2022-06-22 15:58:29 +0100

    remove pylint no module found (#123)

--- kit/commands/remove.py
+++ kit/commands/remove.py
@@ -5,10 +5,7 @@
 
 from shutil import rmtree
 from os import listdir
-from utils.tab_completion import (  # pylint: disable=no-name-in-module
-    components_completer,
-    instances_completer,
-)
+from utils.tab_completion import components_completer, instances_completer
 
 
 def remove_components(args):
