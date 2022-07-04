a30595fa751891e7c7112893acfc6e27b0a7ca80
Hamish Hunt <hamish.hunt@intel.com>
2022-07-04 08:12:46 -0700

    ptxt object

---
+++ expr/scripts/decode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+""""""
+
+
+def decode():
+    """"""
+
+
+if __name__ == "__main__":
+    decode()
