358da6ebc8ce31fd15f46851daa7f8f51b741eed
Hamish Hunt <hamish.hunt@intel.com>
2022-07-14 13:33:29 -0700

    pylint error

--- kit/utils/tab_completion.py
+++ kit/utils/tab_completion.py
@@ -3,7 +3,6 @@
 
 """This module provides tab completion if the dependencies are installed"""
 
-from kit.commands.list_cmd import list_dirs
 from kit.utils.config import load_config
 from kit.utils.files import list_dirs
 
