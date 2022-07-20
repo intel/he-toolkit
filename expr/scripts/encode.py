a9f19e6996f40bf57cc966b4d161bc93fd7b554b
Hamish Hunt <hamish.hunt@intel.com>
2022-07-20 07:13:55 -0700

    some basic argparse on encoder

--- expr/scripts/encode.py
+++ expr/scripts/encode.py
@@ -5,14 +5,29 @@
 
 """Encoder Program"""
 
+import argparse
+from typing import List, Dict
 
-def encode():
+
+def load_toml(arg: str) -> Dict:
     """"""
 
 
-def main():
+def parse_args(argv: List[str] = None):
+    """Parse argv either passed in or from the command line"""
+    parser = argparse.ArgumentParser(description="Encoder for client and server sides")
+    parser.add_argument("datafile", type=str, help="data file to encode")
+    parser.add_argument("--params", type=str, help="parameters for encoding")
+    parser.add_argument(
+        "--config", type=load_toml, default=None, help="set composite columns"
+    )
+    return parser.parse_args(argv) if argv else parser.parse_args()
+
+
+def main(args) -> None:
     """"""
 
 
 if __name__ == "__main__":
-    encode()
+    args = parse_args()
+    main(args)
