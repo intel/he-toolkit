2f7f18f321a456359b01fe940cb6fae7dba58873
Hamish Hunt <hamish.hunt@intel.com>
2022-07-21 13:24:50 -0700

    tidy

--- tools/encoder/nibnaf.h
+++ tools/encoder/nibnaf.h
@@ -9,20 +9,17 @@
 #pragma once
 
 #include <algorithm>
+#include <cmath>
 #include <vector>
 
-inline constexpr double signnum(float x) { return (x > 0.0) - (x < 0.0); }
+inline constexpr double signnum(double x) { return (x > 0.0) - (x < 0.0); }
 
-std::vector<int> gap(double theta, double bw, double epsil, int sz) {
-  std::vector<int> res;
-  std::vector<int> l;
-
+inline std::vector<long> gap(double theta, double bw, double epsil, long sz) {
   double sigma = signnum(theta);
-  double t = std::abs(theta);
-  long r;
+
   std::vector<double> a(sz, 0.0);
   for (double t = std::abs(theta); t > epsil;) {
-    r = std::ceil(std::log(t) / std::log(bw));
+    long r = std::ceil(std::log(t) / std::log(bw));
     r -= (std::pow(bw, r) - t > t - std::pow(bw, r - 1));
     double t_minus_po = t - std::pow(bw, r);
 
@@ -30,19 +27,18 @@ std::vector<int> gap(double theta, double bw, double epsil, int sz) {
     sigma *= signnum(t_minus_po);
     t = std::abs(t_minus_po);
   }
-  // We find the smallest exponent
-  long tem = -sz / 2;
-  while (a[tem + sz / 2] == 0) {
-    tem += 1;
+
+  // Find the smallest exponent
+  long tem;
+  for (tem = -sz / 2; a[tem + sz / 2] == 0; ++tem) {
   }
+  tem = std::min(tem, 0L);
 
   // We shift the exponents to turn it into a polynomial.
   std::vector<double> b(sz, 0.0);
-  tem = std::min(tem, 0);
-
-  for (int i = 0; i < sz / 2 - tem; ++i) {
-    res[i] = a[i + tem + sz / 2];
+  for (long i = 0; i < sz / 2 - tem; ++i) {
+    b[i] = a[i + tem + sz / 2];
   }
 
-  return res;
+  return {b.begin(), b.end()};
 }
