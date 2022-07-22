e394dc0803eb8f44938a75b8dce8e1549bde8d68
Hamish Hunt <hamish.hunt@intel.com>
2022-07-22 09:00:02 -0700

    prelim tests

---
+++ tools/encoder/coder.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+class PolyRep;
+
+struct Coder {
+  virtual PolyRep encode(double num) = 0;
+  virtual double decode(const PolyRep& poly_rep) = 0;
+  virtual ~Coder = default;
+};
--- tools/encoder/encoder-source.cpp
+++ tools/encoder/encoder-source.cpp
@@ -3,28 +3,53 @@
 
 // A program that calls the header and returns the coefficients and the
 // corresponding exponents.
-#include <array>
 #include <iostream>
 
 #include "nibnaf.h"
 
-int main() {
-  double theta, bw, epsil;
-  long sz;
-
-  std::cout << "Enter the number to encode: ";
-  std::cin >> theta;
-  std::cout << "Enter the base to encode: ";
-  std::cin >> bw;
-  std::cout << "Enter the amount of precision: ";
-  std::cin >> epsil;
-  std::cout << "Enter the size of the array: ";
-  std::cin >> sz;
-
-  const auto en = gap(theta, bw, epsil, sz);
-
+template <typename T>
+void print_out_results(const T& en) {
   for (long i = 0; i < en.size(); ++i)
     if (en[i] != 0) std::cout << en[i] << ", " << i << std::endl;
+}
+
+void test_integer() {
+  double number = 546;
+  double bw = 3;
+  double epsil = 0.4;
+  long sz = 200;
+
+  const auto en = gap(number, bw, epsil, sz);
+  print_out_results(en);
+}
+
+void test_float() {
+  double number = 546.789;
+  double bw = 3;
+  double epsil = 0.4;
+  long sz = 200;
+
+  const auto en = gap(number, bw, epsil, sz);
+  print_out_results(en);
+}
+
+void test_zero() {
+  double number = 0;
+  double bw = 3;
+  double epsil = 0.4;
+  long sz = 200;
+
+  const auto en = gap(number, bw, epsil, sz);
+  print_out_results(en);
+}
+
+int main(void) {
+  std::cout << "test float\n";
+  test_float();
+  std::cout << "test integer\n";
+  test_integer();
+  std::cout << "test zero\n";
+  test_zero();
 
   return 0;
 }
