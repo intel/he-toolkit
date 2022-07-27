329009b8577a4d221d5c762f364d76d97ad99b55
Hamish Hunt <hamish.hunt@intel.com>
2022-07-27 14:59:01 -0700

    rearranging

--- expr/scripts/util.py
+++ expr/scripts/util.py
@@ -2,14 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """Encoder utility functions"""
-import sys
-import csv
 import math
 from functools import partial
 from collections import namedtuple
 from itertools import zip_longest
-from ptxt import Ptxt, phi, order_of_p, read_params
+from ptxt import Ptxt, Params
 from typing import List, Sequence
+from dataclasses import dataclass
 
 
 def int_to_pd_poly(num: int, base: int, no_coeffs: int) -> List[int]:
@@ -145,27 +144,27 @@ def extend_with_repetitions(ls_of_ls: list, repeat: int):
         ls *= repeat  # NB. objects are repeats not new objects
 
 
-@dataclass(frozen=true)
+@dataclass(frozen=True)
 class Policy:
     encode: str
     composite: int
 
 
-class ClientEncoder:
-    """Encoder for client"""
+@dataclass
+class Encoder:
+    """Encoder Base class"""
 
+    policies: List[Policy]
+    params: Params
+    composite_columns: List[int]
+    column_policies: List[ColumnPolicies]
+    repeat: int = 1  # segment divisor
 
-class ServerEncoder:
-    """Encoder for server"""
+    def __packing(self,):
+        """To be implemented by derived class"""
+        raise NotImplemented
 
-
-def Encode(
-    policies, params, composite_columns, column_policies, for_server, segment_divisor=1
-):
-    """Closure that returns a function that can encode entries."""
-    repeat = segment_divisor  # Alias for clarity
-
-    def encode(entries):
+    def __call__(entries):
         """Encodes the entries. An entry is a dict with columns as attribs."""
         list_of_ptxts = []
         for colname, policy in column_policies.items():
@@ -177,40 +176,106 @@ def Encode(
             list_ptxt_data = round_robin_encode(
                 encode_datum_with_policy, column_data, composite
             )
-            if for_server is True:
-                # For the server, we must expand each datum into a ptxt.
-                list_of_ptxts.extend(
-                    Ptxt(params).insert_repeated_across_slots(data)
-                    for ptxt_data in list_ptxt_data
-                    for data in grouper(ptxt_data, repeat, [])
-                )
-            else:
-                # TODO refactor so that padding is added maybe before encoding
-                for ptxt_data in list_ptxt_data:
-                    # FIXME surely, not required for each item
-                    padding_size_in_segment = (params.nslots // segment_divisor) - len(
-                        ptxt_data
-                    )
-                    if padding_size_in_segment < 0:
-                        max_queries = params.nslots // segment_divisor
-                        raise ValueError(
-                            f"Cannot have '{len(ptxt_data)}' queries more than a segment allows '{max_queries}'"
-                        )
-                    ptxt_data.extend([] for _ in range(padding_size_in_segment))
-
-                extend_with_repetitions(list_ptxt_data, repeat)
-                list_of_ptxts.extend(
-                    Ptxt(params).insert_data(ptxt_data) for ptxt_data in list_ptxt_data
-                )
+        self.__packing()  # Will be either Client or Server
         # Note the above for server has list in column order, we need row order
         cols = sum(v.composite for v in column_policies.values())
         rows = math.ceil(len(entries) / repeat)
-        return transpose(list_of_ptxts, rows, cols) if for_server else list_of_ptxts
+        return list_of_ptxts
 
-    return encode
+
+class ClientEncoder(Encoder):
+    """Encoder for client"""
+
+    def __packing(self,):
+        # TODO refactor so that padding is added maybe before encoding
+        for ptxt_data in list_ptxt_data:
+            # FIXME surely, not required for each item
+            padding_size_in_segment = (params.nslots // segment_divisor) - len(
+                ptxt_data
+            )
+            if padding_size_in_segment < 0:
+                max_queries = params.nslots // segment_divisor
+                raise ValueError(
+                    f"Cannot have '{len(ptxt_data)}' queries more than a segment allows '{max_queries}'"
+                )
+            ptxt_data.extend([] for _ in range(padding_size_in_segment))
+
+        extend_with_repetitions(list_ptxt_data, repeat)
+        list_of_ptxts.extend(
+            Ptxt(params).insert_data(ptxt_data) for ptxt_data in list_ptxt_data
+        )
+
+
+class ServerEncoder(Encoder):
+    """Encoder for server"""
+
+    def __packing():
+        # For the server, we must expand each datum into a ptxt.
+        list_of_ptxts.extend(
+            Ptxt(params).insert_repeated_across_slots(data)
+            for ptxt_data in list_ptxt_data
+            for data in grouper(ptxt_data, repeat, [])
+        )
+
+    def __call__(self, entries):
+        list_of_ptxts = super.__call__(self, entries)
+        return transpose(list_of_ptxts, rows, cols)
 
 
 def how_many_entries_in_file(filename):
     """Return number of lines in a file."""
     with open(filename) as f:
         return sum(1 for _ in f)
+
+
+# def Encode(
+#    policies, params, composite_columns, column_policies, for_server, segment_divisor=1
+# ):
+#    """Closure that returns a function that can encode entries."""
+#    repeat = segment_divisor  # Alias for clarity
+#
+#    def encode(entries):
+#        """Encodes the entries. An entry is a dict with columns as attribs."""
+#        list_of_ptxts = []
+#        for colname, policy in column_policies.items():
+#            exec_policy = policies[policy.encode]
+#            composite = policy.composite
+#            entries_by_column = (entry[colname] for entry in entries)
+#            column_data = composite_split(entries_by_column, composite)
+#            encode_datum_with_policy = partial(encode_datum, policy_to_exec=exec_policy)
+#            list_ptxt_data = round_robin_encode(
+#                encode_datum_with_policy, column_data, composite
+#            )
+#            if for_server is True:
+#                # For the server, we must expand each datum into a ptxt.
+#                list_of_ptxts.extend(
+#                    Ptxt(params).insert_repeated_across_slots(data)
+#                    for ptxt_data in list_ptxt_data
+#                    for data in grouper(ptxt_data, repeat, [])
+#                )
+#            else:
+#                # TODO refactor so that padding is added maybe before encoding
+#                for ptxt_data in list_ptxt_data:
+#                    # FIXME surely, not required for each item
+#                    padding_size_in_segment = (params.nslots // segment_divisor) - len(
+#                        ptxt_data
+#                    )
+#                    if padding_size_in_segment < 0:
+#                        max_queries = params.nslots // segment_divisor
+#                        raise ValueError(
+#                            f"Cannot have '{len(ptxt_data)}' queries more than a segment allows '{max_queries}'"
+#                        )
+#                    ptxt_data.extend([] for _ in range(padding_size_in_segment))
+#
+#                extend_with_repetitions(list_ptxt_data, repeat)
+#                list_of_ptxts.extend(
+#                    Ptxt(params).insert_data(ptxt_data) for ptxt_data in list_ptxt_data
+#                )
+#        # Note the above for server has list in column order, we need row order
+#        cols = sum(v.composite for v in column_policies.values())
+#        rows = math.ceil(len(entries) / repeat)
+#        return transpose(list_of_ptxts, rows, cols) if for_server else list_of_ptxts
+#
+#    return encode
+#
+#
