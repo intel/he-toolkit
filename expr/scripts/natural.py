d7a87faa533c45469065f237724d3d33398b735f
Hamish Hunt <hamish.hunt@intel.com>
2022-07-28 14:12:09 -0700

    pytest fully refactored

--- expr/scripts/natural.py
+++ expr/scripts/natural.py
@@ -1,17 +1,27 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from dataclasses import dataclass
+"""Module simple class representing the positive integers"""
+
+from __future__ import annotations
+from typing import Union
 
 
-@dataclass(frozen=True)
 class Natural:
     """Represent natural numbers including zero a.k.a positive integers."""
 
-    number: int
-
-    def __post_init__(self):
+    def __init__(self, number: Union[int, str]) -> None:
+        self.number = int(number)
         if self.number < 0:
             raise ValueError(
                 f"Number of entries must be a positive integer, not '{self.number}'"
             )
+
+    def __int__(self) -> int:
+        return self.number
+
+    def __le__(self, other_number: Union[Natural, int]) -> bool:
+        return self.number <= int(other_number)
+
+    def __eq__(self, other_number: Union[Natural, int]) -> bool:
+        return self.number == int(other_number)
