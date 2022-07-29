6492388bc373607ccb03138b5050a373978b2f41
Bahattin Yildiz <107148968+bahattin56@users.noreply.github.com>
2022-07-29 13:03:34 -0700

    Updating the tests
    I have updated the tests by adding functions "PrintPolyRep" and "Decoder". PrintPolyRep prints the polynomial corresponding to the number while decoder evaluates the Laurent expansion at the base, giving us back a close approximation of the number.

--- tools/encoder/encoder-source.cpp
+++ tools/encoder/encoder-source.cpp
@@ -8,7 +8,9 @@
 #include <map>
 
 #include "nibnaf.h"
+#include "iomanip"
 
+//Prints out the coefficients and the exponents of the Laurent Expansion
 template <typename T>
 void print_out_results(const T& en) {
   for (const auto& [key, value] : en) {
@@ -16,13 +18,42 @@ void print_out_results(const T& en) {
   }
 }
 
+// Prints out the polynomial emcoding
+template <typename T, typename U, typename W>
+ void PrintPolyRep(const T& en, U mi, W de){
+  for(const auto& [key, value] : en){
+   std::cout<<value<<"x^" << key-mi;
+   if(key-mi!=de)
+     std::cout<<" + ";
+  }
+ }
+
+//Gets back the original number from the coefficients
+template<typename T, typename U>
+double Decoder(const T& en, U b){
+double sum=0;
+ for(const auto& [key, value] :en) {
+  sum+=value*pow(b,key);
+  }
+return sum;
+}
+
+
 void test_integer() {
   double number = 546;
   double bw = 2;
   double epsil = 0.4;
   
   const auto en = gap(number, bw, epsil);
+//Gets back the minimal negative exponent, the maximal degree and the degree of the polynomial
+long frac_exp=(en.begin()->first-std::abs(en.begin()->first))/2;
+long max_exp=std::prev(en.end())->first;
+long deg=max_exp-frac_exp;
   print_out_results(en);
+ std::cout<<std::endl;
+  PrintPolyRep(en, frac_exp, deg);
+  std::cout<<std::endl;
+ std::cout<<std::fixed<<std::setprecision(10)<<Decoder(en,bw)<<std::endl;
 }
 
 void test_float() {
@@ -31,7 +62,15 @@ void test_float() {
   double epsil = 0.4;
 
   const auto en = gap(number, bw, epsil);
+ //Gets back the minimal negative exponent, the maximal degree and the degree of the polynomial
+long frac_exp=(en.begin()->first-std::abs(en.begin()->first))/2;
+long max_exp=std::prev(en.end())->first;
+long deg=max_exp-frac_exp;
   print_out_results(en);
+ std::cout<<std::endl;
+  PrintPolyRep(en, frac_exp, deg);
+  std::cout<<std::endl;
+ std::cout<<std::fixed<<std::setprecision(10)<<Decoder(en,bw)<<std::endl;
 }
 
 void test_zero() {
@@ -40,7 +79,15 @@ void test_zero() {
   double epsil = 0.4;
   
   const auto en = gap(number, bw, epsil);
+  //Gets back the minimal negative exponent, the maximal degree and the degree of the polynomial
+long frac_exp=(en.begin()->first-std::abs(en.begin()->first))/2;
+long max_exp=std::prev(en.end())->first;
+long deg=max_exp-frac_exp;
   print_out_results(en);
+ std::cout<<std::endl;
+  PrintPolyRep(en, frac_exp, deg);
+  std::cout<<std::endl;
+ std::cout<<std::fixed<<std::setprecision(10)<<Decoder(en,bw)<<std::endl;
 }
 
 int main(void) {
