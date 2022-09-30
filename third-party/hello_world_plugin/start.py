af5805c9a95dd21644f622ac6d8fd86f9180fb71
Hamish Hunt <hamish.hunt@intel.com>
2022-09-30 18:20:21 +0100

    packages are being picked up

--- third-party/hello_world_plugin/start.py
+++ third-party/hello_world_plugin/start.py
@@ -6,6 +6,6 @@ from hello_world_plugin.hello import print_msg
 
 def set_hello_subparser(subparsers):
     """create the parser for the hello world plugin"""
-    parser = subparsers.add_parser("hello_world_plugin", description="hello world")
+    parser = subparsers.add_parser("hello-world", description="hello world")
     parser.add_argument("--quote", action="store_true", help="provide some quote")
     parser.set_defaults(fn=print_msg)
