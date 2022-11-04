e601c0be473b868b26660c835723b6126618ddd8
annishao <annis@d85ed30b42be.jf.intel.com>
2022-11-04 14:53:38 -0700

    test commit and signature
    Signed-off-by: annishao <annis@d85ed30b42be.jf.intel.com>

---
+++ third-party/openfhe_plugin/configurator.py
@@ -0,0 +1,41 @@
+class Configurator:
+    OPENFHE_DEVELOPMENT_BRANCH = "v0.9.2"
+    OPENFHE_HEXL_BRANCH = "main"
+
+    OPENFHE_DEVELOPMENT_REPO = "openfhe-development"
+    OPENFHE_REPO = "openfhe-development"
+    OPENFHE_HEXL = ""
+
+    ROOT = ""
+
+    @classmethod
+    def configure(args) -> None:
+        if args.exists in "Yy":
+            # remove ./openfhe-staging
+            print("previous staging directory deleted.")
+        elif args.exists in "Nn":
+            print("Unwilling to proceed - aborting.")
+            exit(1)
+        if (args.of_build) in "Yy":
+            OPENFHE_REPO = "openfhe-release"
+        if args.hexl_build in "Yy":
+            OPENFHE_HEXL = "openfhe-hexl"
+
+    @classmethod
+    def abort():
+        # MSG=$1
+        print("\n")
+        print("ERROR: $MSG")
+        print()
+        print("CC=$CC CXX=$CXX CMAKE_FLAGS=$CMAKE_FLAGS")
+        print()
+        print("abort.")
+        exit(1)
+
+    @classmethod
+    def separator():
+        print("\n")
+        print(
+            "==============================================================================="
+        )
+        print()
--- third-party/openfhe_plugin/start.py
+++ third-party/openfhe_plugin/start.py
@@ -1,16 +1,20 @@
-def set_openfhe_subparser(subparsers):
-    parser = subparsers.add_parser("openfhe", description="enter an integer")
-    parser.add_argument("ARG", help="enter an integer")
-    parser.set_defaults(fn=FIZZBUZZ)
+import os.path
+from os import path
 
-def FIZZBUZZ(args) -> None:
-    val = int(args.ARG)
-    print("hello")
-    if val % 15 == 0:
-        print("FizzBuzz")
-    elif val % 3 == 0:
-        print("Fizz")
-    elif val % 5 == 0:
-        print("Buzz")
-    else:
-        print(val)
+from openfhe_plugin.configurator import Configurator
+
+
+def set_openfhe_subparser(subparsers):
+    parser = subparsers.add_parser("openfhe", description="OpenFHE configurator")
+    parser.add_argument(
+        "exists",
+        help="If a previous staging directory exists, delete? [y/n] : ",
+        required=False,
+    )
+    parser.add_argument(
+        "of_build", help="Would you like an OpenFHE Release build?     [y/n] : "
+    )
+    parser.add_argument(
+        "hexl_build", help="Would you like a HEXL build?                 [y/n] : "
+    )
+    parser.set_defaults(fn=Configurator.configure)
