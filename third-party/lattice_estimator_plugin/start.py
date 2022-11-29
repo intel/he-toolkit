bff6814adec5679a0daa39b4d18a0c2910044090
Hamish Hunt <hamish.hunt@intel.com>
2022-11-29 13:25:08 +0000

    WIP init

---
+++ third-party/lattice_estimator_plugin/start.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+def set_lattice_estimator_subparser(subparsers):
+    """create the parser for the lattice estimator plugin"""
+    parser = subparsers.add_parser(
+        "hello-world", description="Albrecht et al.'s Lattice Estimator"
+    )
+    parser.add_argument(
+        "--lang",
+        metavar="language",
+        type=str.upper,
+        default="EN",
+        choices=HelloWorlds.get_available_languages(),
+        help="say hello world in this language",
+    )
+    parser.set_defaults(fn=TBA)
