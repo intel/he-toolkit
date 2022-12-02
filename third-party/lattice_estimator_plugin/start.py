6a65738ae456af7a31472d82e73487a8a1f73f5d
Hamish Hunt <hamish.hunt@intel.com>
2022-12-02 15:07:25 +0000

    pytests pass

--- third-party/lattice_estimator_plugin/start.py
+++ third-party/lattice_estimator_plugin/start.py
@@ -1,18 +1,31 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+from kit.utils.component_builder import install_components_from_recipe_file
+from kit.utils.config import config_required
+
 
 def set_lattice_estimator_subparser(subparsers):
     """create the parser for the lattice estimator plugin"""
     parser = subparsers.add_parser(
         "lattice-estimator", description="Albrecht et al.'s Lattice Estimator"
     )
+    # TODO use proper subcommands
     parser.add_argument(
-        "--lang",
-        metavar="language",
-        type=str.upper,
-        default="EN",
-        choices=HelloWorlds.get_available_languages(),
-        help="say hello world in this language",
+        "cmd",
+        choices=("setup", "run"),
+        help="",
     )
-    parser.set_defaults(fn=TBA)
+    parser.set_defaults(fn=setup)
+
+
+@config_required
+def setup(args):
+    """"""
+    install_components_from_recipe_file(
+        "recipes/install.toml", "fetch", config.repo_location, force=False
+    )
+
+
+def run():
+    """"""
