211f27206bad8bd9b4948db64f4d112c2d498cc5
Hamish Hunt <hamish.hunt@intel.com>
2023-08-23 16:46:50 +0000

    check if image already exists

--- kit/commands/docker_build.py
+++ kit/commands/docker_build.py
@@ -187,7 +187,8 @@ def setup_docker(args):
 
     prev = "ubuntu:22.04"
     # Must make sure we have a platform image
-    docker_tools.pull_base_image(prev)
+    if docker_tools.image_exists(prev):
+        docker_tools.pull_base_image(prev)
 
     for feature, path in features.items():
         print("BUILDING", feature.upper(), "DOCKERFILE ...")
