20c23290fa469152f46fd8cede1bb163cdbd38a8
Hamish Hunt <hamish.hunt@intel.com>
2023-09-27 16:40:56 +0100

    WIP added other conversion funcs

--- tools/encoder/encrypt.h
+++ tools/encoder/encrypt.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 
+#include "balanced.h"
 #include "fractional.h"
 
 namespace hekit::coder {
@@ -29,6 +30,46 @@ FractionalEncodedPoly<SparsePoly> encrypt(
   const auto view = sk.getContext().getView();
   view.decrypt(ctxt, sk, poly);
   return FractionalEncodedPoly{SparsePoly{poly}};
+}
+
+BalancedEncodedPoly<helib::Ctxt> encrypt(
+    const BalancedEncodedPoly<SparsePoly>& encoded_poly,
+    const helib::PubKey& pk) {
+  const auto poly = encoded_poly.poly().expand();
+  const auto view = pk.getContext().getView();
+  helib::Ctxt ctxt(pk);
+  view.encrypt(ctxt, pk, poly);
+  return BalancedEncodedPoly{ctxt, encoded_poly.digit()};
+}
+
+BalancedEncodedPoly<SparsePoly> encrypt(
+    const BalancedEncodedPoly<helib::Ctxt>& encoded_poly,
+    const helib::SecKey& sk) {
+  const auto ctxt = encoded_poly.poly();
+  std::vector<long> poly;
+  const auto view = sk.getContext().getView();
+  view.decrypt(ctxt, sk, poly);
+  return BalancedEncodedPoly{SparsePoly{poly}, encoded_poly.digit()};
+}
+
+BalancedSlotsEncodedPoly<helib::Ctxt> encrypt(
+    const BalancedSlotsEncodedPoly<SparsePoly>& encoded_poly,
+    const helib::PubKey& pk) {
+  const auto poly = encoded_poly.poly().expand();
+  const auto view = pk.getContext().getView();
+  helib::Ctxt ctxt(pk);
+  view.encrypt(ctxt, pk, poly);
+  return BalancedSlotsEncodedPoly{ctxt, encoded_poly.digits()};
+}
+
+BalancedSlotsEncodedPoly<SparsePoly> encrypt(
+    const BalancedSlotsEncodedPoly<helib::Ctxt>& encoded_poly,
+    const helib::SecKey& sk) {
+  const auto ctxt = encoded_poly.poly();
+  std::vector<long> poly;
+  const auto view = sk.getContext().getView();
+  view.decrypt(ctxt, sk, poly);
+  return BalancedSlotsEncodedPoly{SparsePoly{poly}, encoded_poly.digits()};
 }
 
 }  // namespace hekit::coder
