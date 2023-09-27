0e74c7b2a16ad54c26608541bfd6e60f34710245
Hamish Hunt <hamish.hunt@intel.com>
2023-09-27 12:00:53 +0100

    mod test to pack slots

--- tools/encoder/tests/test_balanced_slots.cpp
+++ tools/encoder/tests/test_balanced_slots.cpp
@@ -40,45 +40,58 @@ INSTANTIATE_TEST_SUITE_P(
                       vec_double{0.000000001, 12345, 2345.987}));
 
 struct TestBalancedSlotsArith {
-  double num1;
-  double num2;
   double rw;
   double epsil;
+  std::vector<double> nums1;
+  std::vector<double> nums2;
 };
 
 class BalancedSlotsArith
     : public testing::TestWithParam<TestBalancedSlotsArith> {};
 
 TEST_P(BalancedSlotsArith, testBalancedSlotsAddition) {
-  auto& [num1, num2, rw, epsil] = GetParam();
+  const auto& [rw, epsil, nums1, nums2] = GetParam();
 
   BalancedSlotsParams params{rw, epsil};
   Coder coder(params);
-  const auto& encoded1 = coder.encode({num1});
-  const auto& encoded2 = coder.encode({num2});
+  const auto& encoded1 = coder.encode(nums1);
+  const auto& encoded2 = coder.encode(nums2);
   const auto decoded = coder.decode(encoded1 + encoded2);
 
-  EXPECT_NEAR(num1 + num2, decoded[0], epsil) << num1 << " + " << num2;
+  ASSERT_EQ(nums1.size(), nums2.size());
+  for (long n = 0; n < nums1.size(); ++n) {
+    const auto& num1 = nums1[n];
+    const auto& num2 = nums2[n];
+    EXPECT_NEAR(num1 + num2, decoded[n], epsil) << num1 << " + " << num2;
+  }
 }
 
 TEST_P(BalancedSlotsArith, testBalancedSlotsMultiplication) {
-  auto& [num1, num2, rw, epsil] = GetParam();
+  const auto& [rw, epsil, nums1, nums2] = GetParam();
 
   BalancedSlotsParams params{rw, epsil};
   Coder coder(params);
-  const auto& encoded1 = coder.encode({num1});
-  const auto& encoded2 = coder.encode({num2});
+  const auto& encoded1 = coder.encode(nums1);
+  const auto& encoded2 = coder.encode(nums2);
   const auto decoded = coder.decode(encoded1 * encoded2);
 
-  EXPECT_NEAR(num1 * num2, decoded[0], 3 * epsil) << num1 << " * " << num2;
+  ASSERT_EQ(nums1.size(), nums2.size());
+  for (long n = 0; n < nums1.size(); ++n) {
+    const auto& num1 = nums1[n];
+    const auto& num2 = nums2[n];
+    // (X + espil)*(Y + epsil) = XY + epsil*(X+Y) + espil^2. epsil^2 is negl.
+    const auto error = epsil * (num1 + num2);
+    EXPECT_NEAR(num1 * num2, decoded[n], error) << num1 << " * " << num2;
+  }
 }
 
 double default_epsil = 1e-8;
 
-INSTANTIATE_TEST_SUITE_P(variousParameters, BalancedSlotsArith,
-                         ::testing::Values(
-                             /*integer*/
-                             TestBalancedSlotsArith{234.0, 241.0, 1.2,
-                                                    default_epsil}));
+INSTANTIATE_TEST_SUITE_P(
+    variousParameters, BalancedSlotsArith,
+    ::testing::Values(
+        /*integer*/
+        TestBalancedSlotsArith{1.2, default_epsil, vec_double{234.0, 0.0000001},
+                               vec_double{241.0, 101.001}}));
 
 }  // namespace
