2b73147105250b411966617d04e4667c269d30eb
Hamish Hunt <hamish.hunt@intel.com>
2023-10-25 18:55:44 +0100

    Added add and mult dual sparse tests

--- tools/encoder/tests/test_dual_poly.cpp
+++ tools/encoder/tests/test_dual_poly.cpp
@@ -44,4 +44,65 @@ INSTANTIATE_TEST_SUITE_P(singleNums, TestDualEncodeNum,
                              .nums = std::vector{0.0, 1.0, 2.5, 567.68, 1008.01,
                                                  -1.0, -256.16}}));
 
+struct DualEncodeTwoNum {
+  double rw;
+  double epsil;
+  std::pair<long, long> mod_pair;
+  std::vector<double> a_nums;
+  std::vector<double> b_nums;
+};
+
+class TestDualEncodeOps : public testing::TestWithParam<DualEncodeTwoNum> {};
+
+TEST_P(TestDualEncodeOps, testAdd) {
+  const auto [rw, epsil, mod_pair, a_nums, b_nums] = GetParam();
+  auto dual_coder = DualCoder(BalancedSlotsParams{rw, epsil}, mod_pair);
+
+  const auto a_encoded = dual_coder.encode(a_nums);
+  const auto b_encoded = dual_coder.encode(b_nums);
+
+  const auto sum_encoded = a_encoded + b_encoded;
+
+  const auto decoded = dual_coder.decode(sum_encoded);
+
+  for (long i = 0; i < a_nums.size(); ++i) {
+    const auto& a_num = a_nums[i];
+    const auto& b_num = b_nums[i];
+    EXPECT_NEAR(decoded[i], a_num + b_num, 2 * epsil)
+        << "a = " << a_num << ", b = " << b_num
+        << ", a + b = " << a_num + b_num;
+  }
+}
+
+TEST_P(TestDualEncodeOps, testMult) {
+  const auto [rw, epsil, mod_pair, a_nums, b_nums] = GetParam();
+  auto dual_coder = DualCoder(BalancedSlotsParams{rw, epsil}, mod_pair);
+
+  const auto a_encoded = dual_coder.encode(a_nums);
+  const auto b_encoded = dual_coder.encode(b_nums);
+
+  const auto sum_encoded = a_encoded * b_encoded;
+
+  const auto decoded = dual_coder.decode(sum_encoded);
+
+  for (long i = 0; i < a_nums.size(); ++i) {
+    const auto& a_num = a_nums[i];
+    const auto& b_num = b_nums[i];
+    EXPECT_NEAR(decoded[i], a_num * b_num,
+                (std::abs(a_num) + std::abs(b_num)) * epsil)
+        << "a = " << a_num << ", b = " << b_num
+        << ", a + b = " << a_num + b_num;
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(twoNums, TestDualEncodeOps,
+                         ::testing::Values(DualEncodeTwoNum{
+                             .rw = 1.2,
+                             .epsil = 1e-8,
+                             .mod_pair = {157, 257},
+                             .a_nums = std::vector{0.0, 1.0, 2.5, 567.68,
+                                                   1008.01, -1.0, -256.16},
+                             .b_nums = std::vector{0.0, 1.0, 3.4, -301.54,
+                                                   789.99678, -1.115, 20.20}}));
+
 }  // namespace
